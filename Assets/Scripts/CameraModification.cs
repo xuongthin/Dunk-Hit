8357f32283f0f5df854f35585764630ee1265e42
Cactus Nguyen <xuongthin2000@gmail.com>
2022-08-02 05:02:54 +0700

    Update 8.2

---
+++ Assets/Scripts/CameraModification.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraModification : MonoBehaviour
+{
+    private void Start()
+    {
+        float w = Screen.width;
+        float h = Screen.height;
+
+        float size = (h / w) * 10.8f / 2;
+        Camera.main.orthographicSize = size;
+    }
+}
